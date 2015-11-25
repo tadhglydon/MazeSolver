55a3e80f6be204fd8b11e73029f7b80893d3c607
Tadhg Lydon <talydon@gmail.com>
2015-11-25 20:15:28 +0000

    Back to working version

--- MazeSolver/Maze.cs
+++ MazeSolver/Maze.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MazeSolver
 {
-    public abstract class Maze
+    public class Maze
     {
         private int grid_x;
         private int grid_y;
@@ -13,13 +13,9 @@ namespace MazeSolver
         private int end_x;
         private int end_y;
         private Tile[,] maze;
+        private List<Tuple<int, int>> trace;
 
-        public Maze(string[] input)
-        {
-            Load(input);
-        }
-
-        private void Load(string[] input)
+        public void Load(string[] input)
         {
             //We could validate at this stage but instructions say not to.
             //Assuming input files are valid
@@ -46,9 +42,57 @@ namespace MazeSolver
             }            
         }
 
-        public virtual bool Solve()
+        public bool Solve()
         {
-            return
+            //Object to keep track of the path
+            trace = new List<Tuple<int,int>>();
+            
+            if (!Search(start_x, start_y))
+            {
+                //Couldn't find the end
+                return false; 
+            }
+
+            //Unwind the trace
+            foreach (var cell in trace)
+            {
+                maze[cell.Item1, cell.Item2].SolutionPath = true;
+            }
+            
+            return true;
+        }
+
+        private bool Search(int x, int y)
+        {
+            if (x == end_x && y == end_y)
+            {
+                //Found the end :)
+                return true;
+            }
+            else if (maze[x, y].Type == TileType.Wall)
+            {
+                return false;
+            }
+            else if (maze[x, y].Visited)
+            {
+                //We have been here before
+                return false;
+            }
+            maze[x, y].Visited = true;
+
+            if ((x < grid_x - 1 && Search(x + 1, y)) ||
+                (y < grid_y - 1 && Search(x, y + 1)) ||
+                (y > 0 && Search(x, y - 1)) ||
+                (x > 0 && Search(x - 1, y))
+                )
+            {
+                trace.Add(new Tuple<int, int>(x, y));
+                return true;
+            }
+
+            //Dead end
+            trace.Remove(new Tuple<int, int>(x, y));
+            return false;
         }
 
         public string Show()
